46b878b256f3f4616c7b254cbf42591ed9610adf
Athma Vasi <athma.vasi@protonmail.com>
2023-06-26 20:42:30 -0600

    add referment resource(types.d.ts)

---
+++ src/resources/actions/general/referment/referment.types.d.ts
@@ -0,0 +1,87 @@
+import type { Types } from 'mongoose';
+import type { RequestAfterJWTVerification } from '../auth';
+import type { UserRoles } from '../../../user';
+import type { RefermentDocument } from './referment.model';
+
+interface CreateNewRefermentRequest extends RequestAfterJWTVerification {
+  body: {
+    userInfo: {
+      userId: Types.ObjectId; // referrer userId
+      username: string; // referrer username
+      roles: UserRoles;
+    };
+    candidateFullName: string;
+    candidateEmail: string;
+    candidateContactNumber: string;
+    candidateCurrentJobTitle: string;
+    candidateCurrentCompany: string;
+    candidateLinkedinProfile: string;
+
+    positionReferredFor: string;
+    positionJobDescription: string;
+    referralReason: string;
+    additionalInformation: string;
+    privacyConsent: boolean;
+  };
+}
+
+interface DeleteARefermentRequest extends RequestAfterJWTVerification {
+  params: {
+    refermentId: Types.ObjectId;
+  };
+}
+
+type DeleteAllRefermentsRequest = RequestAfterJWTVerification;
+
+type GetAllRefermentsRequest = RequestAfterJWTVerification;
+
+type GetRefermentsByUserRequest = RequestAfterJWTVerification;
+
+interface GetARefermentRequest extends RequestAfterJWTVerification {
+  body: {
+    userInfo: {
+      userId: Types.ObjectId;
+      username: string;
+      roles: UserRoles;
+    };
+    refermentId: Types.ObjectId;
+  };
+}
+
+interface UpdateRefermentRequest extends RequestAfterJWTVerification {
+  body: {
+    userInfo: {
+      userId: Types.ObjectId;
+      username: string;
+      roles: UserRoles;
+    };
+    candidateFullName: string;
+    candidateEmail: string;
+    candidateContactNumber: string;
+    candidateCurrentJobTitle: string;
+    candidateCurrentCompany: string;
+    candidateLinkedinProfile: string;
+
+    positionReferredFor: string;
+    positionJobDescription: string;
+    referralReason: string;
+    additionalInformation: string;
+    privacyConsent: boolean;
+  };
+}
+
+type RefermentsServerResponse = {
+  message: string;
+  refermentData: Array<RefermentDocument>;
+};
+
+export type {
+  CreateNewRefermentRequest,
+  DeleteARefermentRequest,
+  DeleteAllRefermentsRequest,
+  GetAllRefermentsRequest,
+  GetRefermentsByUserRequest,
+  GetARefermentRequest,
+  UpdateRefermentRequest,
+  RefermentsServerResponse,
+};
