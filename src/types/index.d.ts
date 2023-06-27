f592acba60c4725a5a3d5b01e2aed72c6d3d9681
Athma Vasi <athma.vasi@protonmail.com>
2023-06-27 00:29:52 -0600

    comment the generic DatabaseResponse types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,12 +1,18 @@
 import type { FlattenMaps } from 'mongoose';
 
+/**
+ * these types are used in the database service functions for all resources
+ */
+
 type DatabaseResponse<Document> = Promise<
   (FlattenMaps<Document> &
     Required<{
       _id: Types.ObjectId;
     }>)[]
 >;
-
+/**
+ * where generic type Document is the mongoose document type
+ */
 type DatabaseResponseNullable<Document> = Promise<
   | (FlattenMaps<Document> &
       Required<{
