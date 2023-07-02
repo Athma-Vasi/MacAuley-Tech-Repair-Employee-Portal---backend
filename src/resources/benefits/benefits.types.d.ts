f6d4c7170d7a3b6072208b88cd49e5f62a1f5eaa
Athma Vasi <athma.vasi@protonmail.com>
2023-07-02 13:34:53 -0600

    add benefits types.d.ts

---
+++ src/resources/benefits/benefits.types.d.ts
@@ -0,0 +1,65 @@
+import type { Request } from 'express';
+import type { Types } from 'mongoose';
+import type { RequestAfterJWTVerification } from '../auth';
+import type { UserRoles } from '../user';
+import type { BenefitsDocument, BenefitsPlanKind } from './benefits.model';
+
+// RequestAfterJWTVerification extends Request interface from express and adds the decoded JWT (which is the userInfo object) from verifyJWT middleware to the request body
+
+interface CreateNewBenefitsRequest extends RequestAfterJWTVerification {
+  body: {
+    userInfo: {
+      userId: Types.ObjectId;
+      username: string;
+      roles: UserRoles;
+    };
+    benefits: {
+      planName: string;
+      planDescription: string;
+      planKind: BenefitsPlanKind;
+      planStartDate: NativeDate;
+      isPlanActive: boolean;
+      monthlyPremium: number;
+      employerContribution: number;
+      employeeContribution: number;
+    };
+  };
+}
+
+interface DeleteABenefitsRequest extends RequestAfterJWTVerification {
+  params: {
+    benefitsId: Types.ObjectId;
+  };
+}
+
+type DeleteAllBenefitsRequest = RequestAfterJWTVerification;
+
+type GetAllBenefitsRequest = RequestAfterJWTVerification;
+
+type GetBenefitsByUserRequest = RequestAfterJWTVerification;
+
+interface GetBenefitsByIdRequest extends RequestAfterJWTVerification {
+  body: {
+    userInfo: {
+      userId: Types.ObjectId;
+      username: string;
+      roles: UserRoles;
+    };
+  };
+  params: { benefitsId: Types.ObjectId };
+}
+
+type BenefitsServerResponse = {
+  message: string;
+  benefitsData: Array<BenefitsDocument>;
+};
+
+export type {
+  CreateNewBenefitsRequest,
+  DeleteABenefitsRequest,
+  DeleteAllBenefitsRequest,
+  GetAllBenefitsRequest,
+  GetBenefitsByUserRequest,
+  GetBenefitsByIdRequest,
+  BenefitsServerResponse,
+};
