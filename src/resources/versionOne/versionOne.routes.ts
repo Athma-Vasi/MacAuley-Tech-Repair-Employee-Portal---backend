2f05b4e81974b093025743cfb897559d928645db
Athma Vasi <athma.vasi@protonmail.com>
2023-07-23 14:47:38 -0600

    hook up repairNoteRouter to versionOneRouter

--- src/resources/versionOne/versionOne.routes.ts
+++ src/resources/versionOne/versionOne.routes.ts
@@ -2,14 +2,14 @@ import { Router } from 'express';
 import { actionsRouter } from '../actions';
 import { commentRouter } from '../comment';
 import { fileUploadRouter } from '../fileUpload';
-import { noteRouter } from '../note';
 import { userRouter } from '../user';
+import { repairNoteRouter } from '../repairNote';
 
 const versionOneRouter = Router();
 // route: /api/v1
 versionOneRouter.use('/actions', actionsRouter);
 versionOneRouter.use('/users', userRouter);
-versionOneRouter.use('/notes', noteRouter);
+versionOneRouter.use('/repair-notes', repairNoteRouter);
 versionOneRouter.use('/file-uploads', fileUploadRouter);
 versionOneRouter.use('/comments', commentRouter);
 
