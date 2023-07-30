adf1515ff24e8848a3843e03e285d219e95f0f41
Athma Vasi <athma.vasi@protonmail.com>
2023-07-29 23:41:46 -0600

    add middlewares and PATCH route

--- src/resources/actions/company/requestResource/requestResource.routes.ts
+++ src/resources/actions/company/requestResource/requestResource.routes.ts
@@ -6,21 +6,29 @@ import {
   getQueriedRequestResourcesHandler,
   getRequestResourceByIdHandler,
   getRequestResourceByUserHandler,
+  updateRequestResourceStatusByIdHandler,
 } from './requestResource.controller';
+import { assignQueryDefaults, verifyRoles } from '../../../../middlewares';
+import { FIND_QUERY_OPTIONS_KEYWORDS } from '../../../../constants';
 
 const requestResourceRouter = Router();
 
+requestResourceRouter.use(verifyRoles());
+
 requestResourceRouter
   .route('/')
-  .get(getQueriedRequestResourcesHandler)
+  .get(assignQueryDefaults(FIND_QUERY_OPTIONS_KEYWORDS), getQueriedRequestResourcesHandler)
   .post(createNewRequestResourceHandler)
   .delete(deleteAllRequestResourcesHandler);
 
 requestResourceRouter
+  .route('/user')
+  .get(assignQueryDefaults(FIND_QUERY_OPTIONS_KEYWORDS), getRequestResourceByUserHandler);
+
+requestResourceRouter
   .route('/:requestResourceId')
   .get(getRequestResourceByIdHandler)
-  .delete(deleteARequestResourceHandler);
-
-requestResourceRouter.route('/user').get(getRequestResourceByUserHandler);
+  .delete(deleteARequestResourceHandler)
+  .patch(updateRequestResourceStatusByIdHandler);
 
 export { requestResourceRouter };
