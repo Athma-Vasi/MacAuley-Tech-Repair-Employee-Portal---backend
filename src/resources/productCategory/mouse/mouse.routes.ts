ce95bfe79653784ce48aaa03e354750a9ba4bddb
Athma Vasi <athma.vasi@protonmail.com>
2024-04-09 15:55:14 -0600

    add schema validation

--- src/resources/productCategory/mouse/mouse.routes.ts
+++ src/resources/productCategory/mouse/mouse.routes.ts
@@ -9,10 +9,18 @@ import {
   updateMouseByIdHandler,
   updateMiceBulkHandler,
 } from "./mouse.controller";
+import { validateSchemaMiddleware } from "../../../middlewares/validateSchema";
+import { createMouseJoiSchema, updateMouseJoiSchema } from "./mouse.validation";
 
 const mouseRouter = Router();
 
-mouseRouter.route("/").get(getQueriedMiceHandler).post(createNewMouseHandler);
+mouseRouter
+  .route("/")
+  .get(getQueriedMiceHandler)
+  .post(
+    validateSchemaMiddleware(createMouseJoiSchema, "mouseSchema"),
+    createNewMouseHandler
+  );
 
 // separate route for safety reasons (as it deletes all documents in the collection)
 mouseRouter.route("/delete-all").delete(deleteAllMiceHandler);
@@ -25,6 +33,6 @@ mouseRouter
   .route("/:mouseId")
   .get(getMouseByIdHandler)
   .delete(deleteAMouseHandler)
-  .patch(updateMouseByIdHandler);
+  .patch(validateSchemaMiddleware(updateMouseJoiSchema), updateMouseByIdHandler);
 
 export { mouseRouter };
---
+++ src/resources/productCategory/mouse/mouse.validation.ts
@@ -0,0 +1,110 @@
+/**
+ * mouseSensor: MouseSensor; // Optical, Laser, etc.
+  mouseDpi: number; // 800, 1600, etc.
+  mouseButtons: number; // 6, 8, etc.
+  mouseColor: string; // Black, White, etc.
+  mouseInterface: PeripheralsInterface; // USB, Bluetooth, etc.
+  additionalFields: {
+    [key: string]: string;
+  };
+ */
+
+import Joi from "joi";
+import {
+  BRAND_REGEX,
+  SERIAL_ID_REGEX,
+  GRAMMAR_TEXTAREA_INPUT_REGEX,
+  CURRENCY_REGEX,
+  PRODUCT_AVAILABILITY_REGEX,
+  WEIGHT_UNIT_REGEX,
+  DIMENSION_UNIT_REGEX,
+  MOUSE_SENSOR_REGEX,
+  COLOR_VARIANT_REGEX,
+  PERIPHERALS_INTERFACE_REGEX,
+} from "../../../regex";
+
+const createMouseJoiSchema = Joi.object({
+  sku: Joi.array().items(Joi.string().optional()).required(),
+  brand: Joi.string().regex(BRAND_REGEX).required(),
+  model: Joi.string().regex(SERIAL_ID_REGEX).required(),
+  description: Joi.string().regex(GRAMMAR_TEXTAREA_INPUT_REGEX).required(),
+  price: Joi.number().required(),
+  currency: Joi.string().regex(CURRENCY_REGEX).required(),
+  availability: Joi.string().regex(PRODUCT_AVAILABILITY_REGEX).required(),
+  quantity: Joi.number().required(),
+  weight: Joi.number().required(),
+  weightUnit: Joi.string().regex(WEIGHT_UNIT_REGEX).required(),
+  length: Joi.number().required(),
+  lengthUnit: Joi.string().regex(DIMENSION_UNIT_REGEX).required(),
+  width: Joi.number().required(),
+  widthUnit: Joi.string().regex(DIMENSION_UNIT_REGEX).required(),
+  height: Joi.number().required(),
+  heightUnit: Joi.string().regex(DIMENSION_UNIT_REGEX).required(),
+  additionalComments: Joi.string().regex(GRAMMAR_TEXTAREA_INPUT_REGEX).required(),
+
+  mouseSensor: Joi.string().regex(MOUSE_SENSOR_REGEX).required(),
+  mouseDpi: Joi.number().required(),
+  mouseButtons: Joi.number().required(),
+  mouseColor: Joi.string().regex(COLOR_VARIANT_REGEX).required(),
+  mouseInterface: Joi.string().regex(PERIPHERALS_INTERFACE_REGEX).required(),
+  additionalFields: Joi.object().required(),
+
+  starRatingsCount: Joi.object({
+    halfStar: Joi.number().required(),
+    oneStar: Joi.number().required(),
+    oneAndHalfStars: Joi.number().required(),
+    twoStars: Joi.number().required(),
+    twoAndHalfStars: Joi.number().required(),
+    threeStars: Joi.number().required(),
+    threeAndHalfStars: Joi.number().required(),
+    fourStars: Joi.number().required(),
+    fourAndHalfStars: Joi.number().required(),
+    fiveStars: Joi.number().required(),
+  }).required(),
+  productReviewIds: Joi.array().items(Joi.string().optional()).required(),
+  uploadedFilesIds: Joi.array().items(Joi.string().optional()).required(),
+});
+
+const updateMouseJoiSchema = Joi.object({
+  sku: Joi.array().items(Joi.string().optional()),
+  brand: Joi.string().regex(BRAND_REGEX),
+  model: Joi.string().regex(SERIAL_ID_REGEX),
+  description: Joi.string().regex(GRAMMAR_TEXTAREA_INPUT_REGEX),
+  price: Joi.number(),
+  currency: Joi.string().regex(CURRENCY_REGEX),
+  availability: Joi.string().regex(PRODUCT_AVAILABILITY_REGEX),
+  quantity: Joi.number(),
+  weight: Joi.number(),
+  weightUnit: Joi.string().regex(WEIGHT_UNIT_REGEX),
+  length: Joi.number(),
+  lengthUnit: Joi.string().regex(DIMENSION_UNIT_REGEX),
+  width: Joi.number(),
+  widthUnit: Joi.string().regex(DIMENSION_UNIT_REGEX),
+  height: Joi.number(),
+  heightUnit: Joi.string().regex(DIMENSION_UNIT_REGEX),
+  additionalComments: Joi.string().regex(GRAMMAR_TEXTAREA_INPUT_REGEX),
+
+  mouseSensor: Joi.string().regex(MOUSE_SENSOR_REGEX),
+  mouseDpi: Joi.number(),
+  mouseButtons: Joi.number(),
+  mouseColor: Joi.string().regex(COLOR_VARIANT_REGEX),
+  mouseInterface: Joi.string().regex(PERIPHERALS_INTERFACE_REGEX),
+  additionalFields: Joi.object(),
+
+  starRatingsCount: Joi.object({
+    halfStar: Joi.number(),
+    oneStar: Joi.number(),
+    oneAndHalfStars: Joi.number(),
+    twoStars: Joi.number(),
+    twoAndHalfStars: Joi.number(),
+    threeStars: Joi.number(),
+    threeAndHalfStars: Joi.number(),
+    fourStars: Joi.number(),
+    fourAndHalfStars: Joi.number(),
+    fiveStars: Joi.number(),
+  }),
+  productReviewIds: Joi.array().items(Joi.string().optional()),
+  uploadedFilesIds: Joi.array().items(Joi.string().optional()),
+});
+
+export { createMouseJoiSchema, updateMouseJoiSchema };
