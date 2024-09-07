daa15062480eadffc6d298980650e62af377c9ef
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2024-09-07 08:26:58 -0700

    refactor event to latest revision

--- src/resources/actions/outreach/event/index.ts
+++ src/resources/actions/outreach/event/index.ts
@@ -8,78 +8,13 @@
 
 import { EventModel } from "./event.model";
 import { eventRouter } from "./event.routes";
-import {
-  createNewEventController,
-  createNewEventsBulkController,
-  deleteAllEventsController,
-  deleteEventController,
-  getEventByIdController,
-  getEventsByUserController,
-  getQueriedEventsController,
-  updateEventByIdController,
-  updateEventsBulkController,
-} from "./event.controller";
-import {
-  createNewEventService,
-  deleteAllEventsService,
-  deleteEventByIdService,
-  getEventByIdService,
-  getQueriedEventsByUserService,
-  getQueriedEventsService,
-  getQueriedTotalEventsService,
-  updateEventByIdService,
-} from "./event.service";
 
-import type {
-  CreateNewEventRequest,
-  CreateNewEventsBulkRequest,
-  DeleteAllEventsRequest,
-  DeleteEventRequest,
-  GetEventByIdRequest,
-  GetQueriedEventsByUserRequest,
-  GetQueriedEventsRequest,
-  UpdateEventByIdRequest,
-  UpdateEventsBulkRequest,
-} from "./event.types";
-import type { EventDocument, EventSchema, EventKind } from "./event.model";
+import type { EventDocument, EventKind, EventSchema } from "./event.model";
 
 /**
  * Exports
  */
 
-export {
-  EventModel,
-  eventRouter,
-  createNewEventController,
-  createNewEventService,
-  createNewEventsBulkController,
-  deleteAllEventsController,
-  deleteAllEventsService,
-  deleteEventByIdService,
-  deleteEventController,
-  getEventByIdController,
-  getEventByIdService,
-  getEventsByUserController,
-  getQueriedEventsByUserService,
-  getQueriedEventsController,
-  getQueriedEventsService,
-  getQueriedTotalEventsService,
-  updateEventByIdService,
-  updateEventByIdController,
-  updateEventsBulkController,
-};
+export { EventModel, eventRouter };
 
-export type {
-  CreateNewEventRequest,
-  CreateNewEventsBulkRequest,
-  DeleteAllEventsRequest,
-  DeleteEventRequest,
-  GetEventByIdRequest,
-  GetQueriedEventsByUserRequest,
-  GetQueriedEventsRequest,
-  UpdateEventByIdRequest,
-  UpdateEventsBulkRequest,
-  EventSchema,
-  EventDocument,
-  EventKind,
-};
+export type { EventDocument, EventKind, EventSchema };
