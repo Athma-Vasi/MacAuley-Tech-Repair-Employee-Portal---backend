2800c1ea9062fbb794f087d0e4a57f51f6ccea98
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2024-09-07 07:53:52 -0700

    refactor requestResource to latest revision

--- src/resources/actions/company/requestResource/index.ts
+++ src/resources/actions/company/requestResource/index.ts
@@ -8,82 +8,21 @@
 
 import { RequestResourceModel } from "./requestResource.model";
 import { requestResourceRouter } from "./requestResource.routes";
-import {
-  createNewRequestResourceController,
-  createNewRequestResourcesBulkController,
-  deleteAllRequestResourcesController,
-  deleteRequestResourceController,
-  getQueriedRequestResourcesController,
-  getRequestResourceByIdController,
-  getRequestResourcesByUserController,
-  updateRequestResourceByIdController,
-  updateRequestResourcesBulkController,
-} from "./requestResource.controller";
-import {
-  createNewRequestResourceService,
-  deleteAllRequestResourcesService,
-  deleteRequestResourceByIdService,
-  getQueriedRequestResourcesByUserService,
-  getQueriedRequestResourcesService,
-  getQueriedTotalRequestResourcesService,
-  getRequestResourceByIdService,
-  updateRequestResourceByIdService,
-} from "./requestResource.service";
 
 import type {
   RequestResourceDocument,
-  RequestResourceSchema,
   RequestResourceKind,
+  RequestResourceSchema,
 } from "./requestResource.model";
-import type {
-  CreateNewRequestResourceRequest,
-  CreateNewRequestResourcesBulkRequest,
-  DeleteAllRequestResourcesRequest,
-  DeleteRequestResourceRequest,
-  GetQueriedRequestResourcesByUserRequest,
-  GetQueriedRequestResourcesRequest,
-  GetRequestResourceByIdRequest,
-  UpdateRequestResourceByIdRequest,
-  UpdateRequestResourcesBulkRequest,
-} from "./requestResource.types";
 
 /**
  * Exports
  */
 
-export {
-  RequestResourceModel,
-  requestResourceRouter,
-  createNewRequestResourceController,
-  createNewRequestResourcesBulkController,
-  deleteAllRequestResourcesController,
-  deleteRequestResourceController,
-  getQueriedRequestResourcesController,
-  getRequestResourceByIdController,
-  getRequestResourcesByUserController,
-  updateRequestResourceByIdController,
-  updateRequestResourcesBulkController,
-  createNewRequestResourceService,
-  deleteAllRequestResourcesService,
-  deleteRequestResourceByIdService,
-  getQueriedRequestResourcesByUserService,
-  getQueriedRequestResourcesService,
-  getQueriedTotalRequestResourcesService,
-  getRequestResourceByIdService,
-  updateRequestResourceByIdService,
-};
+export { RequestResourceModel, requestResourceRouter };
 
 export type {
   RequestResourceDocument,
-  RequestResourceSchema,
   RequestResourceKind,
-  CreateNewRequestResourceRequest,
-  CreateNewRequestResourcesBulkRequest,
-  DeleteAllRequestResourcesRequest,
-  DeleteRequestResourceRequest,
-  GetQueriedRequestResourcesByUserRequest,
-  GetQueriedRequestResourcesRequest,
-  GetRequestResourceByIdRequest,
-  UpdateRequestResourceByIdRequest,
-  UpdateRequestResourcesBulkRequest,
+  RequestResourceSchema,
 };
--- src/resources/actions/company/requestResource/requestResource.routes.ts
+++ src/resources/actions/company/requestResource/requestResource.routes.ts
@@ -1,48 +1,66 @@
 import { Router } from "express";
-import {
-  createNewRequestResourceController,
-  getQueriedRequestResourcesController,
-  getRequestResourcesByUserController,
-  getRequestResourceByIdController,
-  deleteRequestResourceController,
-  deleteAllRequestResourcesController,
-  updateRequestResourceByIdController,
-  createNewRequestResourcesBulkController,
-  updateRequestResourcesBulkController,
-} from "./requestResource.controller";
 import { validateSchemaMiddleware } from "../../../../middlewares/validateSchema";
 import {
   createRequestResourceJoiSchema,
   updateRequestResourceJoiSchema,
 } from "./requestResource.validation";
+import {
+  createNewResourceHandler,
+  deleteAllResourcesHandler,
+  deleteResourceByIdHandler,
+  getQueriedResourcesByUserHandler,
+  getQueriedResourcesHandler,
+  getResourceByIdHandler,
+  updateResourceByIdHandler,
+} from "../../../../handlers";
+import { RequestResourceModel } from "./requestResource.model";
 
 const requestResourceRouter = Router();
 
 requestResourceRouter
   .route("/")
-  .get(getQueriedRequestResourcesController)
+  // @desc   Get all requestResource plans
+  // @route  GET api/v1/actions/company/requestResource
+  // @access Private/Admin/Manager
+  .get(getQueriedResourcesHandler(RequestResourceModel))
+  // @desc   Create a new requestResource plan
+  // @route  POST api/v1/actions/company/requestResource
+  // @access Private/Admin/Manager
   .post(
-    validateSchemaMiddleware(createRequestResourceJoiSchema, "requestResourceSchema"),
-    createNewRequestResourceController
+    validateSchemaMiddleware(createRequestResourceJoiSchema, "schema"),
+    createNewResourceHandler(RequestResourceModel),
   );
 
-requestResourceRouter.route("/delete-all").delete(deleteAllRequestResourcesController);
+// @desc   Delete all requestResource plans
+// @route  DELETE api/v1/actions/company/request-resource/delete-all
+// @access Private/Admin/Manager
+requestResourceRouter.route("/delete-all").delete(
+  deleteAllResourcesHandler(RequestResourceModel),
+);
 
-requestResourceRouter.route("/user").get(getRequestResourcesByUserController);
+// @desc   Get all requestResource plans by user
+// @route  GET api/v1/actions/company/request-resource/user
+// @access Private/Admin/Manager
+requestResourceRouter.route("/user").get(
+  getQueriedResourcesByUserHandler(RequestResourceModel),
+);
 
-// DEV ROUTES
 requestResourceRouter
-  .route("/dev")
-  .post(createNewRequestResourcesBulkController)
-  .patch(updateRequestResourcesBulkController);
-
-requestResourceRouter
-  .route("/:requestResourceId")
-  .get(getRequestResourceByIdController)
-  .delete(deleteRequestResourceController)
+  .route("/:resourceId")
+  // @desc   Get a requestResource plan by ID
+  // @route  GET api/v1/actions/company/request-resource/:resourceId
+  // @access Private/Admin/Manager
+  .get(getResourceByIdHandler(RequestResourceModel))
+  // @desc   Delete a requestResource plan by ID
+  // @route  DELETE api/v1/actions/company/request-resource/:resourceId
+  // @access Private/Admin/Manager
+  .delete(deleteResourceByIdHandler(RequestResourceModel))
+  // @desc   Update a requestResource plan by ID
+  // @route  PATCH api/v1/actions/company/request-resource/:resourceId
+  // @access Private/Admin/Manager
   .patch(
     validateSchemaMiddleware(updateRequestResourceJoiSchema),
-    updateRequestResourceByIdController
+    updateResourceByIdHandler(RequestResourceModel),
   );
 
 export { requestResourceRouter };
