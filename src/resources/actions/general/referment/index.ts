300104404b18f5c7d14aa22dc33184f3de4d2b7b
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2024-09-07 08:12:46 -0700

    refactor referment to latest revision

--- src/resources/actions/general/referment/index.ts
+++ src/resources/actions/general/referment/index.ts
@@ -7,75 +7,12 @@
  */
 import { RefermentModel } from "./referment.model";
 import { refermentRouter } from "./referment.routes";
-import {
-  createNewRefermentController,
-  createNewRefermentsBulkController,
-  deleteAllRefermentsController,
-  deleteRefermentController,
-  getQueriedRefermentsController,
-  getRefermentByIdController,
-  getRefermentsByUserController,
-  updateRefermentByIdController,
-  updateRefermentsBulkController,
-} from "./referment.controller";
-import {
-  createNewRefermentService,
-  deleteAllRefermentsService,
-  deleteRefermentByIdService,
-  getQueriedRefermentsByUserService,
-  getQueriedRefermentsService,
-  getQueriedTotalRefermentsService,
-  getRefermentByIdService,
-  updateRefermentByIdService,
-} from "./referment.service";
 
 import type { RefermentDocument, RefermentSchema } from "./referment.model";
-import type {
-  CreateNewRefermentRequest,
-  CreateNewRefermentsBulkRequest,
-  DeleteAllRefermentsRequest,
-  DeleteRefermentRequest,
-  GetQueriedRefermentsByUserRequest,
-  GetQueriedRefermentsRequest,
-  GetRefermentByIdRequest,
-  UpdateRefermentByIdRequest,
-  UpdateRefermentsBulkRequest,
-} from "./referment.types";
 
 /**
  * Exports
  */
-export {
-  RefermentModel,
-  refermentRouter,
-  createNewRefermentController,
-  createNewRefermentService,
-  createNewRefermentsBulkController,
-  deleteAllRefermentsController,
-  deleteAllRefermentsService,
-  deleteRefermentController,
-  deleteRefermentByIdService,
-  getQueriedRefermentsByUserService,
-  getQueriedRefermentsController,
-  getQueriedRefermentsService,
-  getQueriedTotalRefermentsService,
-  getRefermentByIdController,
-  getRefermentByIdService,
-  getRefermentsByUserController,
-  updateRefermentByIdService,
-  updateRefermentByIdController,
-  updateRefermentsBulkController,
-};
-export type {
-  RefermentSchema,
-  RefermentDocument,
-  CreateNewRefermentRequest,
-  CreateNewRefermentsBulkRequest,
-  DeleteAllRefermentsRequest,
-  DeleteRefermentRequest,
-  GetQueriedRefermentsByUserRequest,
-  GetQueriedRefermentsRequest,
-  GetRefermentByIdRequest,
-  UpdateRefermentByIdRequest,
-  UpdateRefermentsBulkRequest,
-};
+export { RefermentModel, refermentRouter };
+
+export type { RefermentDocument, RefermentSchema };
--- src/resources/actions/general/referment/referment.routes.ts
+++ src/resources/actions/general/referment/referment.routes.ts
@@ -1,48 +1,66 @@
 import { Router } from "express";
-import {
-  createNewRefermentController,
-  getQueriedRefermentsController,
-  getRefermentsByUserController,
-  getRefermentByIdController,
-  deleteRefermentController,
-  deleteAllRefermentsController,
-  updateRefermentByIdController,
-  createNewRefermentsBulkController,
-  updateRefermentsBulkController,
-} from "./referment.controller";
 import { validateSchemaMiddleware } from "../../../../middlewares/validateSchema";
 import {
   createRefermentJoiSchema,
   updateRefermentJoiSchema,
 } from "./referment.validation";
+import {
+  createNewResourceHandler,
+  deleteAllResourcesHandler,
+  deleteResourceByIdHandler,
+  getQueriedResourcesByUserHandler,
+  getQueriedResourcesHandler,
+  getResourceByIdHandler,
+  updateResourceByIdHandler,
+} from "../../../../handlers";
+import { RefermentModel } from "./referment.model";
 
 const refermentRouter = Router();
 
 refermentRouter
   .route("/")
-  .get(getQueriedRefermentsController)
+  // @desc   Get all referments plans
+  // @route  GET api/v1/actions/general/referment
+  // @access Private/Admin/Manager
+  .get(getQueriedResourcesHandler(RefermentModel))
+  // @desc   Create a new referment plan
+  // @route  POST api/v1/actions/general/referment
+  // @access Private/Admin/Manager
   .post(
-    validateSchemaMiddleware(createRefermentJoiSchema, "refermentSchema"),
-    createNewRefermentController
+    validateSchemaMiddleware(createRefermentJoiSchema, "schema"),
+    createNewResourceHandler(RefermentModel),
   );
 
-refermentRouter.route("/delete-all").delete(deleteAllRefermentsController);
+// @desc   Delete all referments plans
+// @route  DELETE api/v1/actions/general/referment/delete-all
+// @access Private/Admin/Manager
+refermentRouter.route("/delete-all").delete(
+  deleteAllResourcesHandler(RefermentModel),
+);
 
-refermentRouter.route("/user").get(getRefermentsByUserController);
+// @desc   Get all referments plans by user
+// @route  GET api/v1/actions/general/referment/user
+// @access Private/Admin/Manager
+refermentRouter.route("/user").get(
+  getQueriedResourcesByUserHandler(RefermentModel),
+);
 
-// DEV ROUTES
 refermentRouter
-  .route("/dev")
-  .post(createNewRefermentsBulkController)
-  .patch(updateRefermentsBulkController);
-
-refermentRouter
-  .route("/:refermentId")
-  .get(getRefermentByIdController)
-  .delete(deleteRefermentController)
+  .route("/:resourceId")
+  // @desc   Get a referment plan by ID
+  // @route  GET api/v1/actions/general/referment/:resourceId
+  // @access Private/Admin/Manager
+  .get(getResourceByIdHandler(RefermentModel))
+  // @desc   Delete a referment plan by ID
+  // @route  DELETE api/v1/actions/general/referment/:resourceId
+  // @access Private/Admin/Manager
+  .delete(deleteResourceByIdHandler(RefermentModel))
+  // @desc   Update a referments plan by ID
+  // @route  PATCH api/v1/actions/general/referment/:resourceId
+  // @access Private/Admin/Manager
   .patch(
     validateSchemaMiddleware(updateRefermentJoiSchema),
-    updateRefermentByIdController
+    updateResourceByIdHandler(RefermentModel),
   );
 
 export { refermentRouter };
