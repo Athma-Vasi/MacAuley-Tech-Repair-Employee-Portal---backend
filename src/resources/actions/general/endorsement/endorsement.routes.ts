22fb63114927e04be00bda3692f5a8d2cab28618
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2024-09-07 08:03:31 -0700

    refactor endorsement to latest revision

--- src/resources/actions/general/endorsement/endorsement.routes.ts
+++ src/resources/actions/general/endorsement/endorsement.routes.ts
@@ -1,48 +1,66 @@
 import { Router } from "express";
-import {
-  createNewEndorsementController,
-  getQueriedEndorsementsController,
-  getEndorsementsByUserController,
-  getEndorsementByIdController,
-  deleteEndorsementController,
-  deleteAllEndorsementsController,
-  updateEndorsementByIdController,
-  createNewEndorsementsBulkController,
-  updateEndorsementsBulkController,
-} from "./endorsement.controller";
 import { validateSchemaMiddleware } from "../../../../middlewares/validateSchema";
 import {
   createEndorsementJoiSchema,
   updateEndorsementJoiSchema,
 } from "./endorsement.validation";
+import {
+  createNewResourceHandler,
+  deleteAllResourcesHandler,
+  deleteResourceByIdHandler,
+  getQueriedResourcesByUserHandler,
+  getQueriedResourcesHandler,
+  getResourceByIdHandler,
+  updateResourceByIdHandler,
+} from "../../../../handlers";
+import { EndorsementModel } from "./endorsement.model";
 
 const endorsementRouter = Router();
 
 endorsementRouter
   .route("/")
-  .get(getQueriedEndorsementsController)
+  // @desc   Get all endorsements plans
+  // @route  GET api/v1/actions/general/endorsement
+  // @access Private/Admin/Manager
+  .get(getQueriedResourcesHandler(EndorsementModel))
+  // @desc   Create a new endorsement plan
+  // @route  POST api/v1/actions/general/endorsement
+  // @access Private/Admin/Manager
   .post(
-    validateSchemaMiddleware(createEndorsementJoiSchema, "endorsementSchema"),
-    createNewEndorsementController
+    validateSchemaMiddleware(createEndorsementJoiSchema, "schema"),
+    createNewResourceHandler(EndorsementModel),
   );
 
-endorsementRouter.route("/delete-all").delete(deleteAllEndorsementsController);
+// @desc   Delete all endorsements plans
+// @route  DELETE api/v1/actions/general/endorsement/delete-all
+// @access Private/Admin/Manager
+endorsementRouter.route("/delete-all").delete(
+  deleteAllResourcesHandler(EndorsementModel),
+);
 
-endorsementRouter.route("/user").get(getEndorsementsByUserController);
+// @desc   Get all endorsements plans by user
+// @route  GET api/v1/actions/general/endorsement/user
+// @access Private/Admin/Manager
+endorsementRouter.route("/user").get(
+  getQueriedResourcesByUserHandler(EndorsementModel),
+);
 
-// DEV ROUTES
 endorsementRouter
-  .route("/dev")
-  .post(createNewEndorsementsBulkController)
-  .patch(updateEndorsementsBulkController);
-
-endorsementRouter
-  .route("/:endorsementId")
-  .get(getEndorsementByIdController)
-  .delete(deleteEndorsementController)
+  .route("/:resourceId")
+  // @desc   Get an endorsement plan by ID
+  // @route  GET api/v1/actions/general/endorsement/:resourceId
+  // @access Private/Admin/Manager
+  .get(getResourceByIdHandler(EndorsementModel))
+  // @desc   Delete an endorsement plan by ID
+  // @route  DELETE api/v1/actions/general/endorsement/:resourceId
+  // @access Private/Admin/Manager
+  .delete(deleteResourceByIdHandler(EndorsementModel))
+  // @desc   Update an endorsements plan by ID
+  // @route  PATCH api/v1/actions/general/endorsement/:resourceId
+  // @access Private/Admin/Manager
   .patch(
-    validateSchemaMiddleware(updateEndorsementJoiSchema, "documentUpdate"),
-    updateEndorsementByIdController
+    validateSchemaMiddleware(updateEndorsementJoiSchema),
+    updateResourceByIdHandler(EndorsementModel),
   );
 
 export { endorsementRouter };
--- src/resources/actions/general/endorsement/index.ts
+++ src/resources/actions/general/endorsement/index.ts
@@ -7,80 +7,16 @@
  */
 import { endorsementRouter } from "./endorsement.routes";
 import { EndorsementModel } from "./endorsement.model";
-import {
-  createNewEndorsementController,
-  createNewEndorsementsBulkController,
-  deleteAllEndorsementsController,
-  deleteEndorsementController,
-  getEndorsementByIdController,
-  getEndorsementsByUserController,
-  getQueriedEndorsementsController,
-  updateEndorsementByIdController,
-  updateEndorsementsBulkController,
-} from "./endorsement.controller";
-import {
-  createNewEndorsementService,
-  deleteAllEndorsementsService,
-  deleteEndorsementByIdService,
-  getEndorsementByIdService,
-  getQueriedEndorsementsByUserService,
-  getQueriedEndorsementsService,
-  getQueriedTotalEndorsementsService,
-  updateEndorsementByIdService,
-} from "./endorsement.service";
 
 import type {
+  EmployeeAttributes,
   EndorsementDocument,
   EndorsementSchema,
-  EmployeeAttributes,
 } from "./endorsement.model";
-import type {
-  CreateNewEndorsementRequest,
-  CreateNewEndorsementsBulkRequest,
-  DeleteAllEndorsementsRequest,
-  DeleteEndorsementRequest,
-  GetEndorsementByIdRequest,
-  GetQueriedEndorsementsByUserRequest,
-  GetQueriedEndorsementsRequest,
-  UpdateEndorsementByIdRequest,
-  UpdateEndorsementsBulkRequest,
-} from "./endorsement.types";
 
 /**
  * exports
  */
-export {
-  EndorsementModel,
-  endorsementRouter,
-  createNewEndorsementController,
-  createNewEndorsementService,
-  createNewEndorsementsBulkController,
-  deleteAllEndorsementsController,
-  deleteAllEndorsementsService,
-  deleteEndorsementByIdService,
-  deleteEndorsementController,
-  getEndorsementByIdController,
-  getEndorsementByIdService,
-  getEndorsementsByUserController,
-  getQueriedEndorsementsByUserService,
-  getQueriedEndorsementsController,
-  getQueriedEndorsementsService,
-  getQueriedTotalEndorsementsService,
-  updateEndorsementByIdService,
-  updateEndorsementByIdController,
-  updateEndorsementsBulkController,
-};
-export type {
-  EndorsementDocument,
-  EndorsementSchema,
-  CreateNewEndorsementRequest,
-  CreateNewEndorsementsBulkRequest,
-  DeleteAllEndorsementsRequest,
-  DeleteEndorsementRequest,
-  GetEndorsementByIdRequest,
-  GetQueriedEndorsementsByUserRequest,
-  GetQueriedEndorsementsRequest,
-  UpdateEndorsementByIdRequest,
-  UpdateEndorsementsBulkRequest,
-  EmployeeAttributes,
-};
+export { EndorsementModel, endorsementRouter };
+
+export type { EmployeeAttributes, EndorsementDocument, EndorsementSchema };
