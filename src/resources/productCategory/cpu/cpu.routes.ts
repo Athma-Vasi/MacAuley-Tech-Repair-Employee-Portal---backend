d8ee977139272d1d47e2c3ab5b90176543208576
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2024-09-07 08:56:56 -0700

    refactor cpu to latest revision

--- src/resources/productCategory/cpu/cpu.routes.ts
+++ src/resources/productCategory/cpu/cpu.routes.ts
@@ -1,38 +1,63 @@
 import { Router } from "express";
-import {
-  createNewCpuBulkController,
-  createNewCpuController,
-  deleteACpuController,
-  deleteAllCpusController,
-  getCpuByIdController,
-  getQueriedCpusController,
-  updateCpuByIdController,
-  updateCpusBulkController,
-} from "./cpu.controller";
 import { validateSchemaMiddleware } from "../../../middlewares/validateSchema";
 import { createCpuJoiSchema, updateCpuJoiSchema } from "./cpu.validation";
+import {
+  createNewResourceHandler,
+  deleteAllResourcesHandler,
+  deleteResourceByIdHandler,
+  getQueriedResourcesByUserHandler,
+  getQueriedResourcesHandler,
+  getResourceByIdHandler,
+  updateResourceByIdHandler,
+} from "../../../handlers";
+import { CpuModel } from "./cpu.model";
 
 const cpuRouter = Router();
 
 cpuRouter
   .route("/")
-  .get(getQueriedCpusController)
+  // @desc   Get all cpus
+  // @route  GET api/v1/product-category/cpu
+  // @access Private/Admin/Manager
+  .get(getQueriedResourcesHandler(CpuModel))
+  // @desc   Create a new cpu
+  // @route  POST api/v1/product-category/cpu
+  // @access Private/Admin/Manager
   .post(
-    validateSchemaMiddleware(createCpuJoiSchema, "cpuSchema"),
-    createNewCpuController
+    validateSchemaMiddleware(createCpuJoiSchema, "schema"),
+    createNewResourceHandler(CpuModel),
   );
 
-// separate route for safety reasons (as it deletes all documents in the collection)
-cpuRouter.route("/delete-all").delete(deleteAllCpusController);
+// @desc   Delete all cpus
+// @route  DELETE api/v1/product-category/cpu/delete-all
+// @access Private/Admin/Manager
+cpuRouter.route("/delete-all").delete(
+  deleteAllResourcesHandler(CpuModel),
+);
 
-// DEV ROUTE
-cpuRouter.route("/dev").post(createNewCpuBulkController).patch(updateCpusBulkController);
+// @desc   Get all cpus by user
+// @route  GET api/v1/product-category/cpu/user
+// @access Private/Admin/Manager
+cpuRouter.route("/user").get(
+  getQueriedResourcesByUserHandler(CpuModel),
+);
 
-// single document routes
 cpuRouter
-  .route("/:cpuId")
-  .get(getCpuByIdController)
-  .delete(deleteACpuController)
-  .patch(validateSchemaMiddleware(updateCpuJoiSchema), updateCpuByIdController);
+  .route("/:resourceId")
+  // @desc   Get a cpu by its ID
+  // @route  GET api/v1/product-category/cpu/:resourceId
+  // @access Private/Admin/Manager
+  .get(getResourceByIdHandler(CpuModel))
+  // @desc   Delete a cpu by its ID
+  // @route  DELETE api/v1/product-category/cpu/:resourceId
+  // @access Private/Admin/Manager
+  .delete(deleteResourceByIdHandler(CpuModel))
+  // @desc   Update a cpu by its ID
+  // @route  PATCH api/v1/product-category/cpu/:resourceId
+  // @access Private/Admin/Manager
+  .patch(
+    validateSchemaMiddleware(updateCpuJoiSchema),
+    updateResourceByIdHandler(CpuModel),
+  );
 
 export { cpuRouter };
--- src/resources/productCategory/cpu/index.ts
+++ src/resources/productCategory/cpu/index.ts
@@ -8,74 +8,11 @@
 import { cpuRouter } from "./cpu.routes";
 import { CpuModel } from "./cpu.model";
 
-import {
-  createNewCpuBulkController,
-  createNewCpuController,
-  deleteACpuController,
-  deleteAllCpusController,
-  getCpuByIdController,
-  getQueriedCpusController,
-  updateCpuByIdController,
-  updateCpusBulkController,
-} from "./cpu.controller";
-
-import {
-  createNewCpuService,
-  deleteACpuService,
-  deleteAllCpusService,
-  getCpuByIdService,
-  getQueriedCpusService,
-  getQueriedTotalCpusService,
-  returnAllCpusUploadedFileIdsService,
-  updateCpuByIdService,
-} from "./cpu.service";
-
 import type { CpuDocument, CpuSchema } from "./cpu.model";
-import type {
-  CreateNewCpuBulkRequest,
-  CreateNewCpuRequest,
-  DeleteACpuRequest,
-  DeleteAllCpusRequest,
-  GetCpuByIdRequest,
-  GetQueriedCpusRequest,
-  UpdateCpuByIdRequest,
-  UpdateCpusBulkRequest,
-} from "./cpu.types";
 
 /**
  * Exports
  */
+export { CpuModel, cpuRouter };
 
-export {
-  CpuModel,
-  cpuRouter,
-  createNewCpuBulkController,
-  createNewCpuController,
-  createNewCpuService,
-  deleteACpuController,
-  deleteACpuService,
-  deleteAllCpusController,
-  deleteAllCpusService,
-  getCpuByIdController,
-  getCpuByIdService,
-  getQueriedCpusController,
-  getQueriedCpusService,
-  getQueriedTotalCpusService,
-  returnAllCpusUploadedFileIdsService,
-  updateCpuByIdController,
-  updateCpuByIdService,
-  updateCpusBulkController,
-};
-
-export type {
-  CpuDocument,
-  CpuSchema,
-  CreateNewCpuBulkRequest,
-  CreateNewCpuRequest,
-  DeleteACpuRequest,
-  DeleteAllCpusRequest,
-  GetCpuByIdRequest,
-  GetQueriedCpusRequest,
-  UpdateCpuByIdRequest,
-  UpdateCpusBulkRequest,
-};
+export type { CpuDocument, CpuSchema };
